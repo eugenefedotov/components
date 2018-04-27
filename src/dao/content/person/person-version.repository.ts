f5a052026bf76ee39d1cc5ff0920761b6904b590
Pavlov, Dmitry A <pavlov@parma.ru>
2018-04-27 18:07:25 +0500

    save

--- src/dao/content/person/person-version.repository.ts
+++ src/dao/content/person/person-version.repository.ts
@@ -1,7 +1,11 @@
-import {EntityRepository, Repository} from 'typeorm';
+import {DeepPartial, EntityRepository, Repository, SaveOptions} from 'typeorm';
 import {PersonVersionEntity} from './person-version.entity';
 
 @EntityRepository(PersonVersionEntity)
 export class PersonVersionRepository extends Repository<PersonVersionEntity> {
 
+    save<T extends DeepPartial<PersonVersionEntity>>(entities: T[], options?: SaveOptions): Promise<T[]>;
+    save<T extends DeepPartial<PersonVersionEntity>>(entity: T, options?: SaveOptions): Promise<T> {
+        return super.save(entity, options);
+    }
 }
