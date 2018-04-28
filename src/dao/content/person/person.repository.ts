b7c588fbda8d2799000213b467558edfe65d3c01
Pavlov, Dmitry A <pavlov@parma.ru>
2018-04-28 14:07:33 +0500

    ---

--- src/dao/content/person/person.repository.ts
+++ src/dao/content/person/person.repository.ts
@@ -1,21 +1,7 @@
 import {EntityRepository, Repository} from 'typeorm';
 import {PersonEntity} from './person.entity';
-import {PersonVersionRepository} from "./person-version.repository";
 
 @EntityRepository(PersonEntity)
 export class PersonRepository extends Repository<PersonEntity> {
-
-    async saveCurrentVersion(person: PersonEntity): Promise<PersonEntity> {
-        let savedPerson = await this.findOne(person.id);
-
-        if (!savedPerson) {
-            savedPerson = this.create();
-        }
-
-        savedPerson.currentVersion.person = savedPerson;
-        savedPerson.currentVersion = await this.manager.getCustomRepository(PersonVersionRepository).save(person.currentVersion);
-
-        return this.save(savedPerson);
-    }
 
 }
