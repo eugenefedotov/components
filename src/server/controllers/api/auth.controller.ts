4aaa28678536eb861d0e9a918993ee06abc2c378
Pavlov, Dmitry A <pavlov@parma.ru>
2018-05-22 11:14:07 +0500

    auth.controller

---
+++ src/server/controllers/api/auth.controller.ts
@@ -0,0 +1,15 @@
+import {Controller, Get, Put} from '@tsed/common';
+
+@Controller('/auth')
+export class AuthController {
+
+    @Get('')
+    async getInfo() {
+
+    }
+
+    @Put('')
+    async doAuth() {
+
+    }
+}
