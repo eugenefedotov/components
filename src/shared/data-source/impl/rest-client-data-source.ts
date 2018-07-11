bad9103379c55ed87ac433ac2a68313afe38fc0c
DES <desure@mail.ru>
2018-07-11 22:33:57 +0500

    .

--- src/shared/data-source/impl/rest-client-data-source.ts
+++ src/shared/data-source/impl/rest-client-data-source.ts
@@ -1,7 +1,8 @@
 import {DataSource} from '../data-source';
 import {DataSourceResponseModel} from '../models/data-source-response.model';
 import {DataSourceRequestModel} from '../models/data-source-request.model';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 export class RestClientDataSource<T> implements DataSource<T> {
 
@@ -10,6 +11,26 @@ export class RestClientDataSource<T> implements DataSource<T> {
     }
 
     getData(request: DataSourceRequestModel<T>): Promise<DataSourceResponseModel<T>> {
-        return this.http.post<DataSourceResponseModel<T>>(`${this.controllerUrl}/get-data`, request).toPromise();
+        return this.http.post<DataSourceResponseModel<T>>(`${this.controllerUrl}/get-data`,
+            request,
+            {
+                params: this.getFakeParams(request)
+            }).toPromise();
+    }
+
+    private getFakeParams(request: DataSourceRequestModel<T>): HttpParams {
+        let params = new HttpParams();
+
+        if (!environment.production) {
+            params = params.append(this.controllerUrl, `${request.offset}:${request.limit}`);
+
+            if (request.filter && request.filter.length) {
+                request.filter.forEach(filter => {
+                    params = params.append(filter.field, filter.values.join(','));
+                });
+            }
+        }
+
+        return params;
     }
 }
