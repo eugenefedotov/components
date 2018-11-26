a7ba5e58d721f4dbfe9090612a82400aed784129
Pavlov Dmitriy <pavlov@parma.ru>
2018-11-26 17:56:10 +0500

    ---

--- src/server/services/exchange-validator.service.ts
+++ src/server/services/exchange-validator.service.ts
@@ -1,11 +1,15 @@
 import {Service} from '@tsed/common';
 import {ExchangeEntity} from '../../dao/exchange/exchange.entity';
+import {getCustomRepository} from 'typeorm';
+import {ExchangeRepository} from '../../dao/exchange/exchange.repository';
 
 @Service()
 export class ExchangeValidatorService {
 
+    // private exchangeRepository = getCustomRepository(ExchangeRepository);
+
     async isValid(exchange: ExchangeEntity): Promise<boolean> {
-        return true;
+        return exchange.toRequisiteType.mask.isValidAndComplete(exchange.toRequisiteValue);
     }
 
 }
