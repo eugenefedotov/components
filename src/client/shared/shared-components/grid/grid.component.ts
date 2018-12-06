e40682d9c5b5f064cd0cb409edeac170c5257887
DES <desure@mail.ru>
2018-12-06 19:17:14 +0500

    virtual-grid

--- src/client/shared/shared-components/grid/grid.component.ts
+++ src/client/shared/shared-components/grid/grid.component.ts
@@ -2,10 +2,11 @@ import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChan
 import {BehaviorSubject, combineLatest} from 'rxjs';
 import {hasAnyChanges} from '../../../../functions/has-any-changes';
 import {arraySum} from '../../../../functions/array-sum';
-import {map, switchMap} from 'rxjs/operators';
+import {distinctUntilChanged, map, switchMap} from 'rxjs/operators';
 import {arrayFillSpaces} from '../../../../functions/array-fill-spaces';
 import {GridSource} from '../../../../shared/classes/grid-source/grid-source';
 import {SlicedDataSource} from '../../../../shared/classes/data-source/impl/sliced-data-source';
+import {arrayEquals} from '../../../../functions/array-equals';
 
 @Component({
     selector: 'app-grid',
@@ -45,7 +46,8 @@ export class GridComponent<T extends Object = any> implements OnInit, OnChanges 
     source$ = new BehaviorSubject<GridSource<T>>(null);
     columns$ = this.source$
         .pipe(
-            switchMap(source => source.getColumns())
+            switchMap(source => source.getColumns()),
+            distinctUntilChanged(arrayEquals)
         );
 
     defaultRowHeight$ = new BehaviorSubject<number>(24);
@@ -62,7 +64,8 @@ export class GridComponent<T extends Object = any> implements OnInit, OnChanges 
                 offset: 0,
                 limit: 0
             })),
-            map(result => result.count)
+            map(result => result.count),
+            distinctUntilChanged()
         );
 
     widths$ = new BehaviorSubject<number[]>([]);
@@ -70,11 +73,13 @@ export class GridComponent<T extends Object = any> implements OnInit, OnChanges 
 
     filledWidths$ = combineLatest(this.widths$, this.defaultColWidth$, this.columns$)
         .pipe(
-            map(([widths, dcw, columns]) => arrayFillSpaces(widths, dcw, columns.length))
+            map(([widths, dcw, columns]) => arrayFillSpaces(widths, dcw, columns.length)),
+            distinctUntilChanged(arrayEquals)
         );
     filledHeights$ = combineLatest(this.heights$, this.defaultRowHeight$, this.rowsCount$)
         .pipe(
-            map(([heights, drh, rowsCount]) => arrayFillSpaces(heights, drh, rowsCount))
+            map(([heights, drh, rowsCount]) => arrayFillSpaces(heights, drh, rowsCount)),
+            distinctUntilChanged(arrayEquals)
         );
 
     scrollTop$ = new BehaviorSubject<number>(0);
@@ -98,58 +103,71 @@ export class GridComponent<T extends Object = any> implements OnInit, OnChanges 
 
     heightsTop$ = combineLatest(this.holdTopRow$, this.filledHeights$)
         .pipe(
-            map(([holdTopRow, heights]) => heights.slice(0, holdTopRow))
+            map(([holdTopRow, heights]) => heights.slice(0, holdTopRow)),
+            distinctUntilChanged(arrayEquals)
         );
     heightsMiddle$ = combineLatest(this.holdTopRow$, this.holdBottomRow$, this.filledHeights$, this.rowsCount$)
         .pipe(
-            map(([holdTopRow, holdBottomRow, heights, rowsCount]) => heights.slice(holdTopRow, rowsCount - holdBottomRow))
+            map(([holdTopRow, holdBottomRow, heights, rowsCount]) => heights.slice(holdTopRow, rowsCount - holdBottomRow)),
+            distinctUntilChanged(arrayEquals)
         );
     heightsBottom$ = combineLatest(this.holdBottomRow$, this.filledHeights$, this.rowsCount$)
         .pipe(
-            map(([holdBottomRow, heights, rowsCount]) => heights.slice(rowsCount - holdBottomRow, rowsCount))
+            map(([holdBottomRow, heights, rowsCount]) => heights.slice(rowsCount - holdBottomRow, rowsCount)),
+            distinctUntilChanged(arrayEquals)
         );
 
     columnsLeft$ = combineLatest(this.columns$, this.holdLeftCol$)
         .pipe(
-            map(([columns, holdLeftCol]) => columns.slice(0, holdLeftCol))
+            map(([columns, holdLeftCol]) => columns.slice(0, holdLeftCol)),
+            distinctUntilChanged(arrayEquals)
         );
     columnsCenter$ = combineLatest(this.columns$, this.holdLeftCol$, this.holdRightCol$)
         .pipe(
-            map(([columns, holdLeftCol, holdRightCol]) => columns.slice(holdLeftCol, columns.length - holdRightCol))
+            map(([columns, holdLeftCol, holdRightCol]) => columns.slice(holdLeftCol, columns.length - holdRightCol)),
+            distinctUntilChanged(arrayEquals)
         );
     columnsRight$ = combineLatest(this.columns$, this.holdRightCol$)
         .pipe(
-            map(([columns, holdRightCol]) => columns.slice(columns.length - holdRightCol))
+            map(([columns, holdRightCol]) => columns.slice(columns.length - holdRightCol)),
+            distinctUntilChanged(arrayEquals)
         );
 
     widthsLeft$ = combineLatest(this.filledWidths$, this.holdLeftCol$)
         .pipe(
-            map(([widths, holdLeftCol]) => widths.slice(0, holdLeftCol))
+            map(([widths, holdLeftCol]) => widths.slice(0, holdLeftCol)),
+            distinctUntilChanged(arrayEquals)
         );
     widthsCenter$ = combineLatest(this.filledWidths$, this.holdLeftCol$, this.holdRightCol$)
         .pipe(
-            map(([widths, holdLeftCol, holdRightCol]) => widths.slice(holdLeftCol, widths.length - holdRightCol))
+            map(([widths, holdLeftCol, holdRightCol]) => widths.slice(holdLeftCol, widths.length - holdRightCol)),
+            distinctUntilChanged(arrayEquals)
         );
     widthsRight$ = combineLatest(this.filledWidths$, this.holdRightCol$)
         .pipe(
-            map(([widths, holdRightCol]) => widths.slice(widths.length - holdRightCol))
+            map(([widths, holdRightCol]) => widths.slice(widths.length - holdRightCol)),
+            distinctUntilChanged(arrayEquals)
         );
 
     holdLeftPx$ = this.widthsLeft$
         .pipe(
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
     holdTopPx$ = this.heightsTop$
         .pipe(
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
     holdRightPx$ = this.widthsRight$
         .pipe(
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
     holdBottomPx$ = this.heightsBottom$
         .pipe(
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
 
     constructor() {
--- src/client/shared/shared-components/virtual-grid/virtual-grid.component.ts
+++ src/client/shared/shared-components/virtual-grid/virtual-grid.component.ts
@@ -1,15 +1,25 @@
-import {AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {
+    AfterViewChecked,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChanges,
+    ViewChild
+} from '@angular/core';
 import {GridSource} from '../../../../shared/classes/grid-source/grid-source';
-import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
-import {filter, map, switchMap, tap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, Subject} from 'rxjs';
+import {distinctUntilChanged, map, switchMap, takeUntil} from 'rxjs/operators';
 import {hasAnyChanges} from '../../../../functions/has-any-changes';
 import {arraySumIndex} from '../../../../functions/array-sum-index';
 import {arraySum} from '../../../../functions/array-sum';
-
-interface RowWithHeight<T> {
-    row: T;
-    height: number;
-}
+import {arrayEquals} from '../../../../functions/array-equals';
+import {GridColumnModel} from '../../../../shared/classes/grid-source/models/grid-column.model';
+import {ScrollBoxComponent} from '../scroll-box/scroll-box.component';
 
 @Component({
     selector: 'app-virtual-grid',
@@ -17,7 +27,7 @@ interface RowWithHeight<T> {
     styleUrls: ['./virtual-grid.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class VirtualGridComponent<T extends Object = any> implements OnInit, OnChanges, AfterViewChecked {
+export class VirtualGridComponent<T extends Object = any> implements OnInit, OnChanges, AfterViewChecked, OnDestroy {
 
     @Input()
     source: GridSource<T>;
@@ -28,20 +38,16 @@ export class VirtualGridComponent<T extends Object = any> implements OnInit, OnC
     @Input()
     heights: number[];
 
+    @ViewChild(ScrollBoxComponent)
+    scrollBox: ScrollBoxComponent;
+
     source$ = new BehaviorSubject<GridSource<T>>(null);
 
     columns$ = this.source$
         .pipe(
             switchMap(source => source ? source.getColumns() : [[]]),
+            distinctUntilChanged(arrayEquals),
             // tap(val => console.log('columns$', val))
-        );
-    rowsCount$ = this.source$
-        .pipe(
-            switchMap(source => source ? source.getData({
-                offset: 0,
-                limit: 0
-            }) : [null]),
-            map(result => result ? result.count : 0)
         );
 
     widths$ = new BehaviorSubject<number[]>([]);
@@ -53,49 +59,58 @@ export class VirtualGridComponent<T extends Object = any> implements OnInit, OnC
     height$ = new BehaviorSubject(0);
     width$ = new BehaviorSubject(0);
 
-    rowOffset$ = combineLatest(this.heights$, this.scrollTop$)
+    visibleRowStart$ = combineLatest(this.heights$, this.scrollTop$)
         .pipe(
-            map(([heights, scrollTop]) => arraySumIndex(heights, scrollTop))
+            map(([heights, scrollTop]) => arraySumIndex(heights, scrollTop)),
+            distinctUntilChanged()
         );
-    rowEnd$ = combineLatest(this.heights$, this.scrollTop$, this.height$)
+    visibleRowEnd$ = combineLatest(this.heights$, this.scrollTop$, this.height$)
         .pipe(
-            map(([heights, scrollTop, height]) => arraySumIndex(heights, scrollTop + height))
+            map(([heights, scrollTop, height]) => arraySumIndex(heights, scrollTop + height)),
+            distinctUntilChanged()
         );
-    colOffset$ = combineLatest(this.widths$, this.scrollLeft$)
+    visibleColStart$ = combineLatest(this.widths$, this.scrollLeft$)
         .pipe(
-            map(([widths, scrollLeft]) => arraySumIndex(widths, scrollLeft))
+            map(([widths, scrollLeft]) => arraySumIndex(widths, scrollLeft)),
+            distinctUntilChanged()
         );
-    colEnd$ = combineLatest(this.widths$, this.scrollLeft$, this.width$)
+    visibleColEnd$ = combineLatest(this.widths$, this.scrollLeft$, this.width$)
         .pipe(
-            map(([widths, scrollLeft, width]) => arraySumIndex(widths, scrollLeft + width))
+            map(([widths, scrollLeft, width]) => arraySumIndex(widths, scrollLeft + width)),
+            distinctUntilChanged()
         );
 
-    paddingLeft$ = combineLatest(this.widths$, this.colOffset$)
+    hiddenLeftPx$ = combineLatest(this.widths$, this.visibleColStart$)
         .pipe(
             map(([widths, offset]) => widths.slice(0, offset)),
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
-    paddingTop$ = combineLatest(this.heights$, this.rowOffset$)
+    hiddenTopPx$ = combineLatest(this.heights$, this.visibleRowStart$)
         .pipe(
             map(([heights, offset]) => heights.slice(0, offset)),
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
-    paddingRight$ = combineLatest(this.widths$, this.colEnd$, this.columns$)
+    hiddenRightPx$ = combineLatest(this.widths$, this.visibleColEnd$, this.columns$)
         .pipe(
             map(([widths, viewEnd, columns]) => widths.slice(viewEnd, columns.length)),
-            map(arraySum)
+            map(arraySum),
+            distinctUntilChanged()
         );
-    paddingBottom$ = combineLatest(this.heights$, this.rowEnd$, this.rowsCount$)
+    hiddenBottomPx$ = combineLatest(this.heights$, this.visibleRowEnd$)
         .pipe(
-            map(([heights, offset, end]) => heights.slice(offset, end)),
-            map(arraySum)
+            map(([heights, offset]) => heights.slice(offset)),
+            map(arraySum),
+            distinctUntilChanged()
         );
-    visibleColumns$ = combineLatest(this.columns$, this.colOffset$, this.colEnd$)
+    visibleColumns$ = combineLatest(this.columns$, this.visibleColStart$)
         .pipe(
-            map(([columns, start, end]) => columns.slice(start, end)),
+            map(([columns, start]) => columns.slice(start)),
+            distinctUntilChanged(arrayEquals),
             // tap(val => console.log('visibleColumns$', val))
         );
-    visibleRows$ = combineLatest(this.source$, this.visibleColumns$, this.rowOffset$, this.rowEnd$)
+    visibleRows$ = combineLatest(this.source$, this.visibleColumns$, this.visibleRowStart$, this.visibleRowEnd$)
         .pipe(
             // tap(val => console.log('visibleRows$ input', val)),
             switchMap(([source, columns, start, end]) => source ? source.getData({
@@ -104,31 +119,65 @@ export class VirtualGridComponent<T extends Object = any> implements OnInit, OnC
                 fields: columns.map(column => column.field)
             }) : [null]),
             map(result => result ? result.items : []),
+            distinctUntilChanged(arrayEquals),
             // tap(val => console.log('visibleRows$', val))
         );
-    visibleWidths$ = combineLatest(this.widths$, this.colOffset$, this.colEnd$)
+    visibleWidths$ = combineLatest(this.widths$, this.visibleColStart$, this.visibleColEnd$)
         .pipe(
-            map(([widths, offset, end]) => widths.slice(offset, end))
+            map(([widths, offset, end]) => widths.slice(offset, end)),
+            distinctUntilChanged(arrayEquals)
         );
-    visibleHeights$ = combineLatest(this.heights$, this.rowOffset$, this.rowEnd$)
+    visibleHeights$ = combineLatest(this.heights$, this.visibleRowStart$, this.visibleRowEnd$)
         .pipe(
             map(([heights, offset, end]) => heights.slice(offset, end)),
+            distinctUntilChanged(arrayEquals),
             // tap(val => console.log('visibleHeights$', val))
         );
 
-    visibleRowsWithHeights$: Observable<RowWithHeight<T>[]> = combineLatest(this.visibleHeights$, this.visibleRows$)
-        .pipe(
-            map(([heights, rows]) => heights.map((height, index) => ({
-                row: rows[index],
-                height: height
-            }))),
-            // tap(val => console.log('visibleRowsWithHeights$', val))
-        );
+    atomState: {
+        rows: T[];
+        columns: GridColumnModel<T>[];
+        widths: number[];
+        heights: number[];
+        hiddenLeftPx: number;
+        hiddenTopPx: number;
+        hiddenRightPx: number;
+        hiddenBottomPx: number;
+    };
 
-    constructor(private elRef: ElementRef<HTMLElement>) {
+    destroy$ = new Subject();
+
+    constructor(private elRef: ElementRef<HTMLElement>, private cdr: ChangeDetectorRef) {
     }
 
     ngOnInit() {
+        combineLatest(
+            this.visibleRows$,
+            this.visibleColumns$,
+            this.visibleWidths$,
+            this.visibleHeights$,
+            this.hiddenLeftPx$,
+            this.hiddenTopPx$,
+            this.hiddenRightPx$,
+            this.hiddenBottomPx$
+        )
+            .pipe(
+                takeUntil(this.destroy$)
+            )
+            .subscribe(([rows, columns, widths, heights, hiddenLeftPx, hiddenTopPx, hiddenRightPx, hiddenBottomPx]) => {
+                this.atomState = {
+                    rows,
+                    columns,
+                    widths,
+                    heights,
+                    hiddenLeftPx,
+                    hiddenTopPx,
+                    hiddenRightPx,
+                    hiddenBottomPx
+                };
+                this.cdr.detectChanges();
+                this.scrollBox.needUpdate$.next();
+            });
     }
 
     ngOnChanges(changes: SimpleChanges): void {
@@ -141,6 +190,11 @@ export class VirtualGridComponent<T extends Object = any> implements OnInit, OnC
         if (hasAnyChanges<VirtualGridComponent>(changes, ['heights'])) {
             this.heights$.next(this.heights);
         }
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 
     ngAfterViewChecked(): void {
